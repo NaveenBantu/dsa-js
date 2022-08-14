beda5d9ff9d462da2739c0cebb241b451a8b19e4
Naveen Bantu <naveen.bantu1@gmail.com>
2022-08-14 19:37:57 +0200

    Create PokerChips.js

---
+++ algorithms/PokerChips.js
@@ -0,0 +1,11 @@
+// Find min. number of chips a Cashier can use to match the Customer requests in a Casino
+// Cashier has only 100, 50, 25, 10, 5, 1 chips at his disposal
+// For example: If a customer asks for $126 - Cashier should give 3 chips (100, 25, 1)
+// Write a function to find the minimum number of chips the cashier can use to match the customer requests.
+
+function pokerChips(value){
+  // Write code below
+}
+
+// Testing the function using the following example:
+pokerChips(273)   //8 Chips (100, 100, 50, 10, 10, 1, 1, 1)
