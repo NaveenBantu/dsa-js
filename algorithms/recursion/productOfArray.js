021cd84799c1846c4a651ed237ee337c2aa807ee
NaveenBantu <naveen.bantu1@gmail.com>
2022-08-18 13:29:04 +0200

    Update example execution in recursion/productOfArray.js

--- algorithms/recursion/productOfArray.js
+++ algorithms/recursion/productOfArray.js
@@ -3,11 +3,14 @@
 // productOfArray([1,2,3]) // 6
 // productOfArray([1,2,3,10]) // 60
 
-function productArr(array){
-    if (array.length === 1) {
-        return array[0];
-    } else{
-        let elem = array.pop();
-        return elem * productArr(array);
-    }
+function productArr(array) {
+  if (array.length === 1) {
+    return array[0];
+  } else {
+    let elem = array.pop();
+    return elem * productArr(array);
+  }
 }
+
+// Example execution
+console.log(productArr([1, 4, 6]));
