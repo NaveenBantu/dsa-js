437a95707130f509b18fbe466c5f7b4f6038f44b
Naveen Bantu <naveen.bantu1@gmail.com>
2022-08-18 13:16:34 +0200

    Create productOfArray.js

---
+++ algorithms/recursion/productOfArray.js
@@ -0,0 +1,13 @@
+// Using Recursion
+// Write a function called productOfArray which takes in an array of numbers and returns the product of them all
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
+
+function productArr(array){
+    if (array.length === 1) {
+        return array[0];
+    } else{
+        let elem = array.pop();
+        return elem * productArr(array);
+    }
+}
