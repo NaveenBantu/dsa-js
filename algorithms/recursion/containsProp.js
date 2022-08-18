20b072a270f44bcb997143ddd3008b4711e02d97
Naveen Bantu <naveen.bantu1@gmail.com>
2022-08-18 13:25:13 +0200

    Create containsProp.js

---
+++ algorithms/recursion/containsProp.js
@@ -0,0 +1,22 @@
+/*
+- Write a function called `contains` that searches for a value in a nested object. It returns true if the object contains that value.
+
+```javascript
+var nestedObject = {
+    data: {
+        info: {
+            stuff: {
+                thing: {
+                    moreStuff: {
+                        magicNumber: 44
+                    }
+                }
+            }
+        }
+    }
+}
+contains(nestedObject, 44) // true
+contains(nestedObject, "foo") // false
+```
+*/
+
