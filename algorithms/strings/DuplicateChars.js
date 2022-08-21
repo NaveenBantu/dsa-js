c7a4fc787d3d7972c9195c56314f64cd2c51baa6
Naveen Bantu <naveen.bantu1@gmail.com>
2022-08-21 08:42:46 +0200

    Adding Hashmap method for String-Most duplicate character

--- algorithms/strings/DuplicateChars.js
+++ algorithms/strings/DuplicateChars.js
@@ -6,10 +6,12 @@ function bruteMostChar(input) {
     let highestCount = 0;
     let mostChar = '';
 
+    // initial loop to assign the character
     for (let i = 0; i < input.length; i++) {
         let char = input[i];
         let count = 0;
 
+        // looping the rest of the characters to count duplicates
         for (let j = i; j < input.length; j++) {
             if (char === input[j]) {
                 count = count + 1;
@@ -27,3 +29,37 @@ function bruteMostChar(input) {
 
 // Executing to test
 bruteMostChar('AbBbbbcad');
+
+
+
+// Hastmap method - O(n)
+// Find most repeated character in a string (case-sensitive)
+// For example: AbBbbbcad - should return "b" as answer
+
+function hashMostChar(input) {
+    let map = {};
+    let highestCount = 0;
+    let mostChar = '';
+
+    for (let i = 0; i < input.length; i++) {
+        let char = input[i];
+
+        // adding the count of characters to the map
+        if (map[char]) {
+            map[char] = map[char] + 1;
+        } else {
+            map[char] = 1;
+        }
+
+        // checking for the highestcount
+        if (map[char] > highestCount) {
+            highestCount = map[char];
+            mostChar = char;
+        }
+    }
+    // console.log('highest count', highestCount);
+    // console.log('most char ', mostChar);
+}
+
+// Executing to test
+hashMostChar('AbBbbbcad');
